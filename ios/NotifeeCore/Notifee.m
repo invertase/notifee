7ac66e6b7d68dbef57d8fb0a86c5f6b28be05cd0
Salakar <mike.diarmid@gmail.com>
2020-01-31 17:40:37 +0000

    iOS - WIP

---
+++ ios/NotifeeCore/Notifee.m
@@ -0,0 +1,17 @@
+//
+//  Notifee.m
+//  NotifeeCore
+//
+//  Created by Mike on 31/01/2020.
+//  Copyright © 2020 Invertase. All rights reserved.
+//
+
+#import "Notifee.h"
+
+@implementation Notifee
+
++ (void)initialize:(NSString *)testString {
+  NSLog(@"NotifeeInitialize, %@", testString);
+}
+
+@end
