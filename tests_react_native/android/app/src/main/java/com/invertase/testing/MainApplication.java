f870b6446a6c34b0db509c1bda7625ae4cfee03a
Salakar <mike.diarmid@gmail.com>
2020-05-08 10:18:28 +0100

    chore: remove flipper from tests app

--- tests_react_native/android/app/src/main/java/com/invertase/testing/MainApplication.java
+++ tests_react_native/android/app/src/main/java/com/invertase/testing/MainApplication.java
@@ -35,34 +35,6 @@ public class MainApplication extends Application implements ReactApplication {
       }
     };
 
-  /**
-   * Loads Flipper in React Native templates. Call this in the onCreate method with something like
-   * initializeFlipper(this, getReactNativeHost().getReactInstanceManager());
-   */
-  private static void initializeFlipper(
-    Context context, ReactInstanceManager reactInstanceManager) {
-    if (BuildConfig.DEBUG) {
-      try {
-        /*
-         We use reflection here to pick up the class that initializes Flipper,
-        since Flipper library is not available in release mode
-        */
-        Class<?> aClass = Class.forName("com.invertase.testing.ReactNativeFlipper");
-        aClass
-          .getMethod("initializeFlipper", Context.class, ReactInstanceManager.class)
-          .invoke(null, context, reactInstanceManager);
-      } catch (ClassNotFoundException e) {
-        e.printStackTrace();
-      } catch (NoSuchMethodException e) {
-        e.printStackTrace();
-      } catch (IllegalAccessException e) {
-        e.printStackTrace();
-      } catch (InvocationTargetException e) {
-        e.printStackTrace();
-      }
-    }
-  }
-
   @Override
   public ReactNativeHost getReactNativeHost() {
     return mReactNativeHost;
@@ -72,7 +44,6 @@ public class MainApplication extends Application implements ReactApplication {
   public void onCreate() {
     super.onCreate();
     SoLoader.init(this, /* native exopackage */ false);
-    initializeFlipper(this, getReactNativeHost().getReactInstanceManager());
     //    // TODO move to jet
 //    DevInternalSettings settings = (DevInternalSettings) getReactNativeHost()
 //      .getReactInstanceManager()
