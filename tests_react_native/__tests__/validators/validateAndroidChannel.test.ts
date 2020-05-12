eeda8f81dfff5fecb36e2f1658d7846f735d4c80
dackers86 <ackers86@hotmail.com>
2020-05-12 14:26:32 +0100

    Added android channel validation tests

---
+++ tests_react_native/__tests__/validators/validateAndroidChannel.test.ts
@@ -0,0 +1,251 @@
+import validateAndroidChannel from '@notifee/react-native/src/validators/validateAndroidChannel';
+import {
+  AndroidChannel,
+  AndroidVisibility,
+} from '@notifee/react-native/src/types/NotificationAndroid';
+import { Importance } from '@notifee/react-native/src/types/Notification';
+
+describe('Validate Android Channel', () => {
+  describe('validateAndroidChannel()', () => {
+    test('returns valid ', async () => {
+      const channel: AndroidChannel = {
+        id: 'id',
+        name: 'name',
+        bypassDnd: false,
+        lights: true,
+        vibration: true,
+        badge: true,
+        importance: Importance.DEFAULT,
+        visibility: AndroidVisibility.PRIVATE,
+      };
+
+      const $ = validateAndroidChannel(channel);
+      expect($.id).toEqual('id');
+      expect($.name).toEqual('name');
+      expect($.bypassDnd).toEqual(false);
+      expect($.lights).toEqual(true);
+      expect($.vibration).toEqual(true);
+      expect($.vibration).toEqual(true);
+      expect($.importance).toEqual(Importance.DEFAULT);
+      expect($.visibility).toEqual(AndroidVisibility.PRIVATE);
+    });
+
+    test('throws an error with an invalid channel ', async () => {
+      expect(() => validateAndroidChannel([] as any)).toThrowError(
+        "'channel' expected an object value.",
+      );
+    });
+
+    test('throws an error with an invalid id ', async () => {
+      const channel: AndroidChannel = {
+        id: 0 as any,
+        name: 'name',
+      };
+
+      expect(() => validateAndroidChannel(channel)).toThrowError(
+        "'channel.id' expected a string value.",
+      );
+    });
+
+    test('throws an error with an invalid id ', async () => {
+      const channel: AndroidChannel = {
+        id: '' as any,
+        name: 'name',
+      };
+
+      expect(() => validateAndroidChannel(channel)).toThrowError(
+        "'channel.id' expected a valid string id.",
+      );
+    });
+
+    test('throws an error with an invalid name', async () => {
+      const channel: AndroidChannel = {
+        id: 'id',
+        name: 0 as any,
+      };
+
+      expect(() => validateAndroidChannel(channel)).toThrowError(
+        "'channel.name' expected a string value.",
+      );
+    });
+
+    test('throws an error with an invalid name ', async () => {
+      const channel: AndroidChannel = {
+        id: 'id',
+        name: '' as any,
+      };
+
+      expect(() => validateAndroidChannel(channel)).toThrowError(
+        "'channel.name' expected a valid channel name.",
+      );
+    });
+
+    test('throws an error with a non boolean badge', async () => {
+      const channel: AndroidChannel = {
+        id: 'id',
+        name: 'name',
+        badge: [] as any,
+      };
+
+      expect(() => validateAndroidChannel(channel)).toThrowError(
+        "'channel.badge' expected a boolean value.",
+      );
+    });
+
+    test('throws an error with a non boolean bypassDnd', async () => {
+      const channel: AndroidChannel = {
+        id: 'id',
+        name: 'name',
+        bypassDnd: [] as any,
+      };
+
+      expect(() => validateAndroidChannel(channel)).toThrowError(
+        "'channel.bypassDnd' expected a boolean value.",
+      );
+    });
+
+    test('throws an error with a non string description', async () => {
+      const channel: AndroidChannel = {
+        id: 'id',
+        name: 'name',
+        description: [] as any,
+      };
+
+      expect(() => validateAndroidChannel(channel)).toThrowError(
+        "'channel.description' expected a string value.",
+      );
+    });
+
+    test('throws an error with a non string description', async () => {
+      const channel: AndroidChannel = {
+        id: 'id',
+        name: 'name',
+        description: [] as any,
+      };
+
+      expect(() => validateAndroidChannel(channel)).toThrowError(
+        "'channel.description' expected a string value.",
+      );
+    });
+
+    test('throws an error with a non boolean lights property', async () => {
+      const channel: AndroidChannel = {
+        id: 'id',
+        name: 'name',
+        lights: [] as any,
+      };
+
+      expect(() => validateAndroidChannel(channel)).toThrowError(
+        "'channel.lights' expected a boolean value.",
+      );
+    });
+
+    test('throws an error with a non boolean vibration property', async () => {
+      const channel: AndroidChannel = {
+        id: 'id',
+        name: 'name',
+        vibration: [] as any,
+      };
+
+      expect(() => validateAndroidChannel(channel)).toThrowError(
+        "'channel.vibration' expected a boolean value.",
+      );
+    });
+
+    test('throws an error with a non string groupId property', async () => {
+      const channel: AndroidChannel = {
+        id: 'id',
+        name: 'name',
+        groupId: [] as any,
+      };
+
+      expect(() => validateAndroidChannel(channel)).toThrowError(
+        "'channel.groupId' expected a string value.",
+      );
+    });
+
+    test('throws an error with an invalid importance property', async () => {
+      const channel: AndroidChannel = {
+        id: 'id',
+        name: 'name',
+        importance: [] as any,
+      };
+
+      expect(() => validateAndroidChannel(channel)).toThrowError(
+        "'channel.importance' expected an Importance value.",
+      );
+    });
+
+    test('throws an error with a non string lightColor property', async () => {
+      const channel: AndroidChannel = {
+        id: 'id',
+        name: 'name',
+        lightColor: [] as any,
+      };
+
+      expect(() => validateAndroidChannel(channel)).toThrowError(
+        "'channel.lightColor' expected a string value.",
+      );
+    });
+
+    test('throws an error with an invalid lightColor property', async () => {
+      const channel: AndroidChannel = {
+        id: 'id',
+        name: 'name',
+        lightColor: 'unknown',
+      };
+
+      expect(() => validateAndroidChannel(channel)).toThrowError(
+        "'channel.lightColor' invalid color. Expected an AndroidColor or hexadecimal string value",
+      );
+    });
+
+    test('throws an error with an invalid visability property', async () => {
+      const channel: AndroidChannel = {
+        id: 'id',
+        name: 'name',
+        visibility: [] as any,
+      };
+
+      expect(() => validateAndroidChannel(channel)).toThrowError(
+        "'channel.visibility' expected visibility to be an AndroidVisibility value.",
+      );
+    });
+
+    test('throws an error with a non string sound property', async () => {
+      const channel: AndroidChannel = {
+        id: 'id',
+        name: 'name',
+        sound: [] as any,
+      };
+
+      expect(() => validateAndroidChannel(channel)).toThrowError(
+        "'channel.sound' expected a string value.",
+      );
+    });
+
+    test('throws an error with an non array based vibrationPattern property', async () => {
+      const channel: AndroidChannel = {
+        id: 'id',
+        name: 'name',
+        vibrationPattern: {} as any,
+      };
+
+      expect(() => validateAndroidChannel(channel)).toThrowError(
+        "'channel.vibrationPattern' expected an array.",
+      );
+    });
+
+    test('throws an error with an non even array vibrationPattern property', async () => {
+      const channel: AndroidChannel = {
+        id: 'id',
+        name: 'name',
+        vibrationPattern: ['test'] as any,
+      };
+
+      expect(() => validateAndroidChannel(channel)).toThrowError(
+        "'channel.vibrationPattern' expected an array containing an even number of positive values.",
+      );
+    });
+  });
+});
