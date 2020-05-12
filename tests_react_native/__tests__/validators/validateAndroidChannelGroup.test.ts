2d0401e8ec00b3f7edfc4b857bc3af864c627970
dackers86 <ackers86@hotmail.com>
2020-05-12 15:07:26 +0100

    Added android channel group validation tests

---
+++ tests_react_native/__tests__/validators/validateAndroidChannelGroup.test.ts
@@ -0,0 +1,62 @@
+import validateAndroidChannelGroup from '@notifee/react-native/src/validators/validateAndroidChannelGroup';
+import { AndroidChannelGroup } from '@notifee/react-native/src/types/NotificationAndroid';
+// import { Importance } from '@notifee/react-native/src/types/Notification';
+
+describe('Validate Android Channel Group', () => {
+  describe('validateAndroidChannelGroup()', () => {
+    test('returns valid ', async () => {
+      const channelGroup: AndroidChannelGroup = {
+        id: 'id',
+        name: 'name',
+        description: 'description',
+      };
+
+      const $ = validateAndroidChannelGroup(channelGroup);
+      expect($.id).toEqual('id');
+      expect($.name).toEqual('name');
+      expect($.description).toEqual('description');
+    });
+
+    test('throws an error with an invalid channel ', async () => {
+      expect(() => validateAndroidChannelGroup([] as any)).toThrowError(
+        "'group' expected an object value.",
+      );
+    });
+
+    test('throws an error with an invalid id ', async () => {
+      const channelGroup: AndroidChannelGroup = {
+        id: [] as any,
+        name: 'name',
+        description: 'description',
+      };
+
+      expect(() => validateAndroidChannelGroup(channelGroup)).toThrowError(
+        "'group.id' expected a string value.",
+      );
+    });
+
+    test('throws an error with an invalid name ', async () => {
+      const channelGroup: AndroidChannelGroup = {
+        id: 'id',
+        name: [] as any,
+        description: 'description',
+      };
+
+      expect(() => validateAndroidChannelGroup(channelGroup)).toThrowError(
+        "'group.name' expected a string value.",
+      );
+    });
+
+    test('throws an error with an invalid description ', async () => {
+      const channelGroup: AndroidChannelGroup = {
+        id: 'id',
+        name: 'name',
+        description: [] as any,
+      };
+
+      expect(() => validateAndroidChannelGroup(channelGroup)).toThrowError(
+        "'group.description' expected a string value.",
+      );
+    });
+  });
+});
