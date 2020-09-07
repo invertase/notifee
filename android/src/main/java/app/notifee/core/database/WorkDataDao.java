0501d77ddbb18d7611bb82078e4b7bbc4be18393
Helena Ford <helenaellieford@gmail.com>
2020-09-07 09:30:25 +0100

    cleanup

--- android/src/main/java/app/notifee/core/database/WorkDataDao.java
+++ android/src/main/java/app/notifee/core/database/WorkDataDao.java
@@ -7,13 +7,16 @@ import androidx.room.Query;
 
 @Dao
 public interface WorkDataDao {
-  // allowing the insert of the same word multiple times by passing a
-  // conflict resolution strategy
+  // TODO: check if this is the correct strategy to update an existing entity
   @Insert(onConflict = OnConflictStrategy.IGNORE)
   void insert(WorkDataEntity word);
 
   @Query("DELETE FROM work_data")
   void deleteAll();
+
+  // TODO: implement delete to clear requests once complete
+//  @Query("DELETE FROM work_data WHERE id = :id")
+//  void delete(String id);
 
   @Query("SELECT * from work_data WHERE id = :id")
   WorkDataEntity getWorkDataById(String id);
