f346598974b47b234f0c3e490f9dc9632cbe1813
Helena Ford <helenaellieford@gmail.com>
2020-10-08 16:08:34 +0100

    fix(android): openBatteryOptimizationSettings exceptions fix

---
+++ android/src/main/java/app/notifee/core/utility/IntentUtils.java
@@ -0,0 +1,41 @@
+package app.notifee.core.utility;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
+import app.notifee.core.ContextHolder;
+import app.notifee.core.Logger;
+import java.util.List;
+
+public class IntentUtils {
+  private static final String TAG = "IntentUtils";
+
+  public static boolean isAvailableOnDevice(Context ctx, Intent intent) {
+    try {
+      if (ctx == null || intent == null) {
+        return false;
+      }
+
+      final PackageManager mgr = ctx.getPackageManager();
+      List<ResolveInfo> list = mgr.queryIntentActivities(intent, PackageManager.MATCH_DEFAULT_ONLY);
+      return list.size() > 0;
+    } catch (Exception e) {
+      Logger.e(TAG, "An error occurred whilst trying to check if intent is available on device", e);
+
+      return false;
+    }
+  }
+
+  public static void startActivityOnUiThread(Activity activity, Intent intent) {
+    activity.runOnUiThread(
+      () -> {
+        try {
+          ContextHolder.getApplicationContext().startActivity(intent);
+        } catch (Exception e) {
+          Logger.e(TAG, "An error occurred whilst trying to start activity on Ui Thread", e);
+        }
+      });
+  }
+}
--- android/src/main/java/app/notifee/core/utility/PowerManagerUtils.java
+++ android/src/main/java/app/notifee/core/utility/PowerManagerUtils.java
@@ -7,17 +7,36 @@ import android.os.Build;
 import android.os.PowerManager;
 import android.provider.Settings;
 import app.notifee.core.ContextHolder;
+import app.notifee.core.Logger;
 
 public class PowerManagerUtils {
+  private static final String TAG = "PowerManagerUtils";
+
   public static void openBatteryOptimizationSettings(Activity activity) {
     if (Build.VERSION.SDK_INT < Build.VERSION_CODES.M) {
       return;
     }
-    Intent intent = new Intent();
-    intent.setAction(Settings.ACTION_IGNORE_BATTERY_OPTIMIZATION_SETTINGS);
-    intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-    activity.runOnUiThread(() -> ContextHolder.getApplicationContext().startActivity(intent));
+
+    try {
+      Intent intent = new Intent();
+      intent.setAction(Settings.ACTION_IGNORE_BATTERY_OPTIMIZATION_SETTINGS);
+      intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+
+      // check activity exists on device
+      if (activity != null) {
+        Boolean isAvailableOnDevice =
+          IntentUtils.isAvailableOnDevice(ContextHolder.getApplicationContext(), intent);
+        if (!isAvailableOnDevice) {
+          return;
+        }
+
+        IntentUtils.startActivityOnUiThread(activity, intent);
+      }
+    } catch (Exception e) {
+      Logger.e(TAG, "An error occurred whilst trying to open battery optimization settings", e);
+    }
   }
+
 
   public static Boolean isBatteryOptimizationEnabled(Context context) {
     if (Build.VERSION.SDK_INT < Build.VERSION_CODES.M) {
