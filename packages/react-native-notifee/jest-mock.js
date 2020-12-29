d8fcd9ed89b7c7efff19472a13ea46f1aaf1b214
Mike Hardy <github@mikehardy.net>
2020-12-29 09:36:17 -0500

    test(jest): provide generic jest mock for users
    Thanks to @joshuacano for helping develop and test this

---
+++ packages/react-native-notifee/jest-mock.js
@@ -0,0 +1,25 @@
+/* eslint-disable no-undef */
+import * as ReactNative from 'react-native';
+
+jest.doMock('react-native', () => {
+  return Object.setPrototypeOf(
+    {
+      Platform: {
+        OS: 'android',
+        // eslint-disable-next-line @typescript-eslint/explicit-function-return-type
+        select: () => {
+          /* do nothing */
+        },
+      },
+      NativeModules: {
+        ...ReactNative.NativeModules,
+        NotifeeApiModule: {
+          addListener: jest.fn(),
+          eventsAddListener: jest.fn(),
+          eventsNotifyReady: jest.fn(),
+        },
+      },
+    },
+    ReactNative,
+  );
+});
