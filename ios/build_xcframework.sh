f9cc29c3ed1721d557a06f84bd7b77933ea75819
Mike Hardy <github@mikehardy.net>
2021-01-05 21:10:03 -0500

    fix(ios, catalyst): fix framework structure for catalyst

--- ios/build_xcframework.sh
+++ ios/build_xcframework.sh
@@ -26,6 +26,8 @@ xcodebuild archive -workspace "./${FRAMEWORK_NAME}.xcworkspace" -scheme ${BUILD_
   -destination="iOS" -archivePath "${IOS_DEVICE_ARCHIVE_PATH}" \
   -sdk iphoneos SKIP_INSTALL=NO BUILD_LIBRARY_FOR_DISTRIBUTION=YES | xcpretty -k
 
+# Do not be tempted to use "-sdk macosx" instead of the "destination" argument. It switches you to AppKit from UIKit
+# https://developer.apple.com/forums/thread/120542?answerId=374124022#374124022
 echo "Archiving for Mac Catalyst"
 xcodebuild archive -workspace "./${FRAMEWORK_NAME}.xcworkspace" -scheme ${BUILD_SCHEME} -configuration Release \
   -destination='platform=macOS,arch=x86_64,variant=Mac Catalyst' -archivePath "${CATALYST_ARCHIVE_PATH}" \
@@ -42,9 +44,15 @@ rm -rf "${SIMULATOR_ARCHIVE_PATH}"
 rm -rf "${IOS_DEVICE_ARCHIVE_PATH}"
 rm -rf "${CATALYST_ARCHIVE_PATH}"
 
+# Catalyst framework directory structure flattening step 1:
+# - Copy the framework in a specific way: de-referencing symbolic links on purpose
 echo "Installing framework into packages/react-native submodule"
 rm -rf "${OUTPUT_FOLDER}/${FRAMEWORK_NAME}.xcframework" && \
-  cp -a "${FRAMEWORK_PATH}" "${OUTPUT_FOLDER}"
+  cp -rp "${FRAMEWORK_PATH}" "${OUTPUT_FOLDER}"
+
+# Catalyst framework directory structure flattening step 2:
+# - Remove the catalyst framework "Versions" directory structure after symbolic link de-reference
+rm -rf "${OUTPUT_FOLDER}/${FRAMEWORK_NAME}.xcframework/ios-arm64_x86_64-maccatalyst/${FRAMEWORK_NAME}.framework/Versions"
 
 echo "Cleaning up intermediate files"
 rm -rf "${WORKING_DIR:?}/${FRAMEWORK_FOLDER_NAME}"
