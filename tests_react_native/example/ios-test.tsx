9753fe2511e146ce792e212caeaa5777277bec27
Helena Ford <helenaellieford@gmail.com>
2021-05-14 15:00:05 +0100

    test: update default empty strings tests (#85)

--- tests_react_native/example/ios-test.tsx
+++ tests_react_native/example/ios-test.tsx
@@ -86,6 +86,18 @@ async function testRunner(): Promise<void> {
       categoryId: 'test_category',
     },
   });
+
+  await Notifee.displayNotification({
+    title: undefined,
+    body: undefined,
+  });
+
+  await Notifee.displayNotification({
+    title: '',
+    body: '',
+  });
+
+  await Notifee.displayNotification({});
   console.log('Pressed');
 }
 
