92a507306ef73e5c848bf7f1c48f8635438ea192
Mike Hardy <github@mikehardy.net>
2021-06-14 15:53:46 -0500

    build: warn if not using Xcode 12.4, commit module built w/12.4
    Xcode generates frameworks that are not backwards compatible to older versions of Xcode...sometimes.
    
    The rule appears to be that you must not change Xcode version used to build a library unless you have to,
    and when you do you must signal as a breaking change in release notes as it might be a breaking change
    for library consumers

--- ios/build_xcframework.sh
+++ ios/build_xcframework.sh
@@ -1,6 +1,38 @@
 #!/bin/bash
 set -eo pipefail
 
+
+XCODE_WANTED_VERSION="Xcode 12"
+XCODE_WANTED_VERSION_COMPLETE="${XCODE_WANTED_VERSION}.4"
+echo "Verifying Xcode installation (want $XCODE_WANTED_VERSION_COMPLETE)"
+if ! XCODE_VERSION=$(xcodebuild -version|grep Xcode|cut -f1 -d'.') || [ "$XCODE_VERSION" != "$XCODE_WANTED_VERSION" ]; then
+  echo "Xcode not installed or not correct version, got $XCODE_VERSION, expected $XCODE_WANTED_VERSION_COMPLETE"
+  echo "Open the App Store app, and either install or update Xcode to $XCODE_WANTED_VERSION_COMPLETE"
+  echo "If you are sure you have Xcode installed but it says you have a CommandLineTools intance, do 'sudo xcode-select -r' to fix"
+  exit 1
+fi
+XCODE_VERSION=$(xcodebuild -version|grep Xcode)
+RED='\033[0;31m'
+YELLOW='\033[0;33m'
+NC='\033[0m' # No Color
+if [ "$XCODE_VERSION" != "$XCODE_WANTED_VERSION_COMPLETE" ]; then
+  echo
+  echo -e ${RED}!!!!!!!!!!!!!!!!!!!!!!!!!!!${NC}
+  echo -e ${RED}!!!!!!!!!!!!!!!!!!!!!!!!!!!${NC}
+  echo
+  echo -e ${YELLOW}You may know what you are doing, so this is just a warning, but${NC}
+  echo
+  echo -e "${YELLOW}Supported Xcode version is: $XCODE_WANTED_VERSION_COMPLETE${NC}"
+  echo -e "${YELLOW}Your Xcode version is:      $XCODE_VERSION${NC}"
+  echo
+  echo -e ${YELLOW}You may break customer builds if you publish the results of this unsupported Xcode version.${NC}
+  echo
+  echo -e ${RED}!!!!!!!!!!!!!!!!!!!!!!!!!!!${NC}
+  echo -e ${RED}!!!!!!!!!!!!!!!!!!!!!!!!!!!${NC}
+  echo
+  sleep 20
+fi
+
 WORKING_DIR=$(pwd)
 FRAMEWORK_FOLDER_NAME="Notifee_XCFramework"
 FRAMEWORK_NAME="NotifeeCore"
