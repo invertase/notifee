74f37b9a98d503250ca056639c5f1903aba7a4a4
Helena Ford <helenaellieford@gmail.com>
2021-09-25 23:46:10 +0100

    chore: remove config script on iOS and update github link

--- packages/react-native/react-native.config.js
+++ packages/react-native/react-native.config.js
@@ -5,15 +5,7 @@ module.exports = {
         packageImportPath: 'import io.invertase.notifee.NotifeePackage;',
         packageInstance: 'new NotifeePackage()',
       },
-      ios: {
-        scriptPhases: [
-          {
-            name: '[NOTIFEE] Config',
-            path: './ios_config.sh',
-            execution_position: 'after_compile',
-          },
-        ],
-      },
+      ios: {},
     },
   },
 };
