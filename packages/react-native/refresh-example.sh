5dab2e9a343e7449c58d8dbc7240f8570300246d
Mike Hardy <github@mikehardy.net>
2021-10-05 11:59:37 -0500

    docs(example): fix refresh-example.sh script and refresh example
    tested and working ios + android locally

--- packages/react-native/refresh-example.sh
+++ packages/react-native/refresh-example.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 set -e
 
-echo "You should run this from directory where you have cloned the @notifee/react-native repo"
+echo "You should run this from the packages/react-native directory inside the cloned invertase/notifee repo"
 echo "You should only do this when your git working set is completely clean (e.g., git reset --hard)"
-echo "You must run \`npm install react-native-cli -g\` prior to running this script"
 echo "You must have already run \`yarn\` in the repository so \`npx react-native\` will work"
-echo "This scaffolding refresh has been tested on macOS, if you use it on linux, it might not work"
+echo "This scaffolding refresh has been tested on macOS, not on windows or linux"
 
 # Copy the important files out temporarily
 if [ -d TEMP ]; then
@@ -36,24 +35,29 @@ npx react-native init example
 pushd example
 echo "Adding Notifee app package"
 yarn add github:notifee/react-native-notifee
+sed -i -e $'s/mavenLocal()/mavenLocal()\\\n        maven \{ url "$rootDir\/..\/node_modules\/@notifee\/react-native\/android\/libs" \}/' android/build.gradle
+rm -f android/build.gradle??
 
 # add post install step for notifee
 npx json -I -f package.json -e 'this.scripts.postinstall = "cd node_modules/@notifee/react-native && yarn"'
 
 # cd node_modules/@notifee/react-native && yarn add genversion && cd ../../..
 
-# react-native 0.60 is cocoapods mainly now, so run pod install after installing @notifee/react-native
+# This is just a speed optimization, very optional, but asks xcodebuild to use clang and clang++ without the fully-qualified path
+# That means that you can then make a symlink in your path with clang or clang++ and have it use a different binary
+# In that way you can install ccache or buildcache and get much faster compiles...
+sed -i -e $'s/react_native_post_install(installer)/react_native_post_install(installer)\\\n\\\n    installer.pods_project.targets.each do |target|\\\n      target.build_configurations.each do |config|\\\n        config.build_settings["CC"] = "clang"\\\n        config.build_settings["LD"] = "clang"\\\n        config.build_settings["CXX"] = "clang++"\\\n        config.build_settings["LDPLUSPLUS"] = "clang++"\\\n      end\\\n    end/' ios/Podfile
+rm -f ios/Podfile??
+
 echo "Installing pods"
-cd ios && pod install && cd ..
+npx pod-install
 
 # remove App.js in favour of our custom App.tsx
 rm App.js
 
 # We use typescript and there are linter collisions with transitive dependencies on old versions
 # Merge the result of a PR we made upstream so lint is clean even with our 3-deep layer of packages
-yarn add @react-native-community/eslint-config@^3
-
-rm -f ios/Podfile??
+yarn add --dev @react-native-community/eslint-config@^3
 
 yarn
 
