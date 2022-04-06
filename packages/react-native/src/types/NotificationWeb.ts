f7ea279ddf3020ae67e4aba248504aa8b4aaee0f
Mike Hardy <github@mikehardy.net>
2022-04-06 16:24:53 -0500

    feat(web): add web types for notification settings
    empty now, but lays the groundwork for real features

---
+++ packages/react-native/src/types/NotificationWeb.ts
@@ -0,0 +1,5 @@
+/**
+ * Empty at the moment but will contain web-specific settings as needed
+ */
+// eslint-disable-next-line @typescript-eslint/no-empty-interface
+export interface WebNotificationSettings {}
