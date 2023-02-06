ba62dcc5d9c585c6dc848c2130fc0cb831e451a4
gosunman <gksrmf382@naver.com>
2023-02-06 21:23:02 +0900

    docs: fix link and typo 'behviour#full-screen-action' (#672)

--- packages/react-native/src/types/Notification.ts
+++ packages/react-native/src/types/Notification.ts
@@ -251,7 +251,7 @@ export interface NotificationPressAction {
  * On Android; when provided to a notification action, the action will only open you application if
  * a `launchActivity` and/or a `mainComponent` is provided.
  *
- * Please see the [FullScreen Action](/react-native/docs/android/behaviour#full-screen-action) document to learn more.
+ * Please see the [FullScreen Action](/react-native/docs/android/behaviour#full-screen) document to learn more.
  */
 export interface NotificationFullScreenAction {
   /**
@@ -268,7 +268,7 @@ export interface NotificationFullScreenAction {
    * This property can be used in advanced scenarios to launch a custom Android Activity when the user
    * performs a full-screen action.
    *
-   * View the [Android Full Screen](/react-native/docs/android/behviour#full-screen-action) docs to learn more.
+   * View the [Android Full Screen](/react-native/docs/android/behaviour#full-screen) docs to learn more.
    *
    * @platform android
    */
@@ -289,7 +289,7 @@ export interface NotificationFullScreenAction {
    * This property can be used to open a custom React component when the notification is displayed.
    * For this to correctly function on Android, a minor native code change is required.
    *
-   * View the [Full-screen Action](/react-native/docs/android/behviour#full-screen-action) document to learn more.
+   * View the [Full-screen Action](/react-native/docs/android/behaviour#full-screen) document to learn more.
    *
    * @platform android
    */
