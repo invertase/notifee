14532243e834bc9dcb64119d58c08743d780020c
Helena Ford <helenaellieford@gmail.com>
2023-03-13 16:12:32 +0000

    -

--- packages/react-native/src/NotifeeJSEventEmitter.ts
+++ packages/react-native/src/NotifeeJSEventEmitter.ts
@@ -5,8 +5,7 @@
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-ignore
 import EventEmitter from 'react-native/Libraries/vendor/emitter/EventEmitter';
-import { EventEmitter as EventEmitterInterface } from 'react-native';
 
 // @ts-ignore See https://github.com/facebook/react-native/pull/36462
-const emitter: EventEmitterInterface = new EventEmitter();
+const emitter = new EventEmitter();
 export default emitter;
