420489548fb29a4ea61383331d48559b85cf1e91
Helena Ford <helenaellieford@gmail.com>
2023-03-13 16:40:19 +0000

    fix: ts error with react native 0.71 (#709)

--- packages/react-native/example/android/app/src/main/java/com/example/MainApplication.java
+++ packages/react-native/example/android/app/src/main/java/com/example/MainApplication.java
@@ -2,7 +2,6 @@ package com.example;
 
 import android.app.Application;
 import android.content.Context;
-import com.example.newarchitecture.MainApplicationReactNativeHost;
 import com.facebook.react.PackageList;
 import com.facebook.react.ReactApplication;
 import com.facebook.react.ReactInstanceManager;
@@ -10,6 +9,7 @@ import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.config.ReactFeatureFlags;
 import com.facebook.soloader.SoLoader;
+import com.example.newarchitecture.MainApplicationReactNativeHost;
 import java.lang.reflect.InvocationTargetException;
 import java.util.List;
 
