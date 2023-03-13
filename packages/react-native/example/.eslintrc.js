247c9a2a36b5d2a8d9a44e58f8a1b097e1209e3e
Helena Ford <helenaellieford@gmail.com>
2023-03-13 16:39:16 +0000

    -

--- packages/react-native/example/.eslintrc.js
+++ packages/react-native/example/.eslintrc.js
@@ -2,7 +2,7 @@ module.exports = {
   root: true,
   extends: '@react-native-community',
   parser: '@typescript-eslint/parser',
-  plugins: ['@typescript-eslint'],
+  plugins: ['@typescript-eslint', 'jest'],
   overrides: [
     {
       files: ['*.ts', '*.tsx'],
