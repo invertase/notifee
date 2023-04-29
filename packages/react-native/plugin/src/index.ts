c441b9a5011fdac4332ca4bc6b5b907a6fff3e88
Jakov Glavina <jakov.glavina@icloud.com>
2023-04-29 22:30:08 +0200

    chore: update Expo installation docs & remove config plugin (#763)

--- packages/react-native/plugin/src/index.ts
+++
@@ -1,9 +0,0 @@
-import { ConfigPlugin, withPlugins } from '@expo/config-plugins';
-
-import withNotifeeProjectGradlePlugin from './withNotifeeProjectGradlePlugin';
-
-const withNotifee: ConfigPlugin = config => {
-  return withPlugins(config, [withNotifeeProjectGradlePlugin]);
-};
-
-export default withNotifee;
