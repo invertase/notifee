5357a709d30a9ad7669d5fa09c24d254cc2c031c
Mike Hardy <github@mikehardy.net>
2024-12-15 19:04:16 -0500

    test: add timestamp and background handler test
    now there is a button that will create a trigger notification
    for +15 secs, allowing time to kill the app. Then a background
    handler should tell you that you got a notification in the background

--- tests_react_native/index.js
+++ tests_react_native/index.js
@@ -1,5 +1,10 @@
 import { AppRegistry } from 'react-native';
+import notifee from '@notifee/react-native';
 
 import App from './example/app';
+
+notifee.onBackgroundEvent(async event => {
+  console.log('notifee.onBackgroundEvent triggered: ' + JSON.stringify(event));
+});
 
 AppRegistry.registerComponent('testing', () => App);
