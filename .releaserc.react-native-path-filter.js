3b22d91aefb1951930676e97f0b46de5678b44d5
Mike Hardy <github@mikehardy.net>
2024-12-16 13:34:09 -0500

    chore: implement semantic release (w/custom path filter)
    should allow for automated releases despite this repo being shared
    with flutter
    
    - use custom path filter for git-log command
    - specify header format for react-native release notes
    - specify the version-related files for git commit
    - use conventional commits format (not angular format)

---
+++ .releaserc.react-native-path-filter.js
@@ -0,0 +1,21 @@
+path = require('path');
+
+const reactNativePaths = ['ios', 'android', 'packages/react-native'];
+
+Object.keys(require.cache)
+  .filter(m => path.posix.normalize(m).endsWith('/node_modules/git-log-parser/src/index.js'))
+  .forEach(moduleName => {
+    const parse = require.cache[moduleName].exports.parse;
+    require.cache[moduleName].exports.parse = (
+      config,
+      options,
+    ) => {
+      // set the `_` "raw value(s)" param of git-log-parser config to be an array
+      // of raw values containing whatever is in there now, along with our filter paths
+      // at the end so we only get git-log for those paths
+      if (Array.isArray(config._)) config._ = config._.concat(reactNativePaths);
+      else if (config._) config._ = [config._, ...reactNativePaths];
+      else config._ = reactNativePaths;
+      return parse(config, options);
+    };
+  });
