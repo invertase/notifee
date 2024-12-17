68f230f32ec75e993521b0feb8f9358fb008d3e5
Mike Hardy <github@mikehardy.net>
2024-12-17 15:14:30 -0500

    fix(ios): copy iOS NotifeeCore library more carefully during build
    there was the possibility on certain OSes (Ubuntu-22 it seems) that
    bash would copy into sub-directories with casual usage of paths
    
    now the copy is definitely to an absolute path and won't create erroneous
    nested directories regardless of initial state
    
    also it will print what it's copying, for later verification

--- build_ios_core.sh
+++ build_ios_core.sh
@@ -3,14 +3,12 @@ set -eo pipefail
 
 WORKING_DIR=$(pwd)
 POD_NAME="NotifeeCore"
-POD_PATH="${WORKING_DIR}/ios/${POD_NAME}/"
-POD_OUTPUT_FOLDER=${WORKING_DIR}/packages/react-native/ios/NotifeeCore/
+POD_PATH="${WORKING_DIR}/ios/${POD_NAME}"
+POD_OUTPUT_FOLDER_BASE=${WORKING_DIR}/packages/react-native/ios
 
-rm -rf "${POD_OUTPUT_FOLDER}"
-echo "Deleted ${POD_OUTPUT_FOLDER}"
-mkdir "${POD_OUTPUT_FOLDER}"
-echo "Created ${POD_OUTPUT_FOLDER}"
+rm -rf "${POD_OUTPUT_FOLDER_BASE:?}/${POD_NAME}"
+echo "Deleted ${POD_OUTPUT_FOLDER_BASE:?}/${POD_NAME}"
 
 # - Copy the core pod
 echo "Moving core into packages/react-native submodule"
-cp -rp "${POD_PATH}" "${POD_OUTPUT_FOLDER}"
+cp -rvp "${POD_PATH}" "${POD_OUTPUT_FOLDER_BASE}/"
